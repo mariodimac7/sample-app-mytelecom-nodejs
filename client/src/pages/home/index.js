d87156e548926538ede83cce28bfa57fcbeddf2b
Mario Dimac <34098891+mariodimac7@users.noreply.github.com>
2023-12-15 15:38:54 +0000

    Update Home.json and index.js

--- client/src/pages/home/index.js
+++ client/src/pages/home/index.js
@@ -52,6 +52,44 @@ const Home = () => {
           </Row>
         </div>
       </div>
+      <div className="container-fluid">
+        <div className="justify-content-center text-center d-flex">
+          <Row className="cards-container">
+            <Card
+              additionalClass="card-1"
+              img={img1}
+              title={t('Card1.Title')}
+              description={t('Card1.Description')}
+              linkTo="/assumptionLiability"
+              getStarted={t('Card1.Button')}
+              featureTitle={t('Card1.FeaturesTitle')}
+              featureList={t('Card1.FeaturesList')}
+            />
+            <div className="w-100 d-block d-md-none" />
+            <Card
+              additionalClass="card-2"
+              img={img2}
+              title={t('Card2.Title')}
+              description={t('Card2.Description')}
+              linkTo="/purchaseDevice"
+              getStarted={t('Card2.Button')}
+              featureTitle={t('Card2.FeaturesTitle')}
+              featureList={t('Card2.FeaturesList')}
+            />
+            <div className="w-100 d-block d-md-none" />
+            <Card
+              additionalClass="card-3"
+              img={img3}
+              title={t('Card3.Title')}
+              description={t('Card3.Description')}
+              linkTo="/serviceChange"
+              getStarted={t('Card3.Button')}
+              featureTitle={t('Card3.FeaturesTitle')}
+              featureList={t('Card3.FeaturesList')}
+            />
+          </Row>
+        </div>
+      </div>
       <PreFooter />
     </>
   );
