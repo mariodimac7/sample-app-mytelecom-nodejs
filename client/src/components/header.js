b5a85c33ad0e121c9dec82998c2d6d26aee546bc
Mario Dimac <34098891+mariodimac7@users.noreply.github.com>
2023-12-15 14:31:28 +0000

    Update header styles and content

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -3,23 +3,38 @@ import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import logo from '../assets/img/logo.png';
 import source from '../assets/img/github-source.svg';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 export const Header = () => {
   const { t } = useTranslation('Common');
 
   return (
-    <header className="header" role="banner">
-      <nav className="navbar navbar-expand-md navbar-light bg-light">
-        <Link className="navbar-brand" to="/">
-          <img src={logo} alt="logo" />
-        </Link>
-
-        <div className="justify-content-end">
-          <a className="nav-link" href={t('GitHubLink')} rel="noopener noreferrer" target="_blank">
-            <img src={source} alt={t('GitHubLinkText')} />
-          </a>
-        </div>
-      </nav>
-    </header>
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">DocuSign Demo Sample App</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home"></Nav.Link>
+            <Nav.Link href="#link"></Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+        <Form inline>
+          <Row>
+            <Col xs="auto">
+              <Button type="submit">CLM</Button>
+            </Col>
+          </Row>
+        </Form>
+      </Container>
+    </Navbar>
   );
 };
